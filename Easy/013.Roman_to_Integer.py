299404cecc99a810a180a00630ed3717a717825a
Göksu Turaç <turacgoksu@gmail.com>
2024-05-02 03:05:31 +0300

    13th problem solved

---
+++ Easy/013.Roman_to_Integer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        char_list = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        result = 0
+        for char in s:
+            result += char_list[char]
+        return result
